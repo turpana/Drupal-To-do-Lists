ac1ec9f791a45a8dd5c74eb590a89d84b5010a13
tko <tko@739222.no-reply.drupal.org>
2012-02-10 17:46:32 -0700

    Switched to all entitys, no content types. Added template files

---
+++ theme/todo-list.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file todo-list.tpl.php
+ * Todo list template
+ *
+ * Variables
+ * - $todo_items: rendered list of items
+ */
+?>
+<div class="todo-list">
+<?php print $items; ?>
+</div>
---
+++ theme/todo-lists.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file todo-lists.tpl.php
+ * Todo_lists template
+ *
+ * Variables
+ * - $lists: rendered list of todo_lists
+ */
+?>
+<div class="todo-lists">
+<?php print $lists; ?>
+</div>
